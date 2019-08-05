f4d730d94148aed1360088890011b530681a7d6a
Mikhail Berezin <mvberezin@avito.ru>
2019-08-05 10:50:53 +0300

    misc

--- App.js
+++ App.js
@@ -45,12 +45,12 @@ async function loadResourcesAsync() {
       ...Ionicons.font,
       // We include SpaceMono because we use it in HomeScreen.js. Feel free to
       // remove this if you are not using it in your app
-      'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
-    }),
+      'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf')
+    })
   ]);
 }
 
-function handleLoadingError(error: Error) {
+function handleLoadingError(error) {
   // In this case, you might want to report the error to your error reporting
   // service, for example Sentry
   console.warn(error);
@@ -64,5 +64,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff'
-  },
+  }
 });
