590bb7ef7b6562c309b5f9c714d2fa80a9cdea03
Mikhail Berezin <mvberezin@avito.ru>
2019-08-20 16:57:42 +0300

    remove react-native-elements

--- screens/MatchScreen.js
+++ screens/MatchScreen.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
-import { Text } from 'react-native-elements';
+import { Text } from 'react-native';
 
 function MatchScreen() {
   return (
