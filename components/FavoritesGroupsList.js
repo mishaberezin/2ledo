3edb75366afa4d26beccb2e75467b701a4623ae1
Ilya <ilya@MacBook-Pro-admin.local>
2019-08-31 23:04:41 +0300

    Add plate with content

--- components/FavoritesGroupsList.js
+++ components/FavoritesGroupsList.js
@@ -1,20 +1,14 @@
 import React from 'react';
-import { ScrollView } from 'react-native';
+import { ScrollView, View } from 'react-native';
 
 import FavoritesList from '../components/FavoritesList';
 
-const FavoritesGroupsList = ({ groups }) => {
-  return (
-    <ScrollView>
-      {groups.map(group => (
-        <FavoritesList
-          key={group.title}
-          title={group.title}
-          items={group.items}
-        />
-      ))}
-    </ScrollView>
-  );
+const FavoritesGroupsList = ({ groups, withScroll }) => {
+  const list = groups.map(group => (
+    <FavoritesList key={group.title} title={group.title} items={group.items} />
+  ));
+
+  return withScroll ? <ScrollView>{list}</ScrollView> : <View>{list}</View>;
 };
 
 export default FavoritesGroupsList;
