365ace34b820a039c92f61cbc3f38f40846e5575
Ilya <ilya@MacBook-Pro-admin.local>
2019-08-31 21:15:55 +0300

    merge master

--- components/HederButton.js
+++ components/HederButton.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { HeaderButton } from 'react-navigation-header-buttons';
 
 import { Ionicons } from '@expo/vector-icons';
-import Colors from '../constants/Colors';
+import COLORS from '../constants/colors';
 
 const CustomHeaderButton = props => (
   <HeaderButton
     {...props}
     IconComponent={Ionicons}
     iconSize={23}
-    color={Colors.tintColor}
+    color={COLORS.tintColor}
   />
 );
 
