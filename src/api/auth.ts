0e3f556a96d972f7e006bc60a4750eb03e5f0cc8
Ilya Gribov <iagribov@avito.ru>
2020-07-01 14:16:01 +0300

    add axios interceptor

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -2,12 +2,13 @@ import ExpoConstants from "expo-constants";
 import { apiAxios } from "./axios";
 import { storeToken } from "./token-storage";
 
-export const loginByApplicationId = async () => {
-  const { data } = await apiAxios.post("/auth/login", {
-    clientId: ExpoConstants.installationId,
-    secret: "",
-  });
+export const requestToken = () => apiAxios.post("/auth/login", {
+  clientId: ExpoConstants.installationId,
+  secret: "",
+});
 
+export const loginByApplicationId = async () => {
+  const { data } = await requestToken();
   const { token } = data;
 
   await storeToken(token);
